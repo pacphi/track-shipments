b0d2cd0a065439e3b9fffabdef3196a438e175b1
Chris Phillipson <cphillipson@pivotal.io>
2017-12-18 09:39:48 -0800

    Update setup/teardown of CF accounts, org and spaces

--- scripts/create-environments.sh
+++ scripts/create-environments.sh
@@ -5,12 +5,16 @@
 
 # Variables
 ORG=scm
-SPACE1=test
-SPACE2=staging
-SPACE3=production
+SPACE1=sandbox
+SPACE2=test
+SPACE3=staging
+SPACE4=production
 CF_USER=joedev
 CF_PASSWORD=passw0rd
+CF_CI_USER=ci-daemon
+CF_CI_PASSWORD=passw0rd
 ORG_ROLE=OrgManager
+ORG_ROLE_RO=OrgAuditor
 SPACE_ROLE=SpaceDeveloper
 
 
@@ -21,16 +25,23 @@ cf create-org $ORG
 cf create-space $SPACE1 -o $ORG
 cf create-space $SPACE2 -o $ORG
 cf create-space $SPACE3 -o $ORG
+cf create-space $SPACE4 -o $ORG
 
 # Create users
 cf create-user $CF_USER $CF_PASSWORD
-cf set-org-role $CF_USER $ORG $ORG_ROLE
+cf set-org-role $CF_USER $ORG $ORG_ROLE_RO
 cf set-space-role $CF_USER $ORG $SPACE1 $SPACE_ROLE
 cf set-space-role $CF_USER $ORG $SPACE2 $SPACE_ROLE
 cf set-space-role $CF_USER $ORG $SPACE3 $SPACE_ROLE
 
+cf create-user $CF_CI_USER $CF_CI_PASSWORD
+cf set-org-role $CF_CI_USER $ORG $ORG_ROLE
+cf set-space-role $CF_CI_USER $ORG $SPACE2 $SPACE_ROLE
+cf set-space-role $CF_CI_USER $ORG $SPACE3 $SPACE_ROLE
+cf set-space-role $CF_CI_USER $ORG $SPACE4 $SPACE_ROLE
+
 # Target staging
-cf target -o $ORG -s $SPACE2
+cf target -o $ORG -s $SPACE3
 
 # Create auto-scale service instance in staging
 cf create-service app-autoscaler standard i-autoscale-you
--- scripts/destroy-environments.sh
+++ scripts/destroy-environments.sh
@@ -6,6 +6,7 @@
 # Variables
 ORG=scm
 CF_ACCOUNT=joedev
+CF_CI_ACCOUNT=ci-daemon
 
 
 # Do not change anything beyond here unless you know what you're doing.
@@ -15,3 +16,4 @@ cf delete-org $ORG -f
 
 # Delete user
 cf delete-user $CF_ACCOUNT -f
+cf delete-user $CF_CI_ACCOUNT -f
