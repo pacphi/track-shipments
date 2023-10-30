2910b7592044b1ce2fe975aad5983dca6287bfe8
Chris Phillipson <cphillipson@vmware.com>
2023-10-30 10:59:24 -0700

    Move @EnableLoadTimeWeaving * to @Configuration annotated class * also revert logging provider to Logback * tests now PASS

--- src/main/java/io/pivotal/track/TrackShipmentsMvpApplication.java
+++ src/main/java/io/pivotal/track/TrackShipmentsMvpApplication.java
@@ -2,10 +2,8 @@ package io.pivotal.track;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
 @SpringBootApplication
-@EnableLoadTimeWeaving
 public class TrackShipmentsMvpApplication {
 
 	public static void main(String[] args) {
