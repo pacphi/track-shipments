61e4c808b38e02d3e5d5986f2b6d1321ae68371b
Chris Phillipson <cphillipson@vmware.com>
2023-10-30 10:22:55 -0700

    Add HAL explorer; tests still fail for apparent lack of SimpleJpaRepository

--- src/main/java/io/pivotal/track/TrackingStatusRepository.java
+++ src/main/java/io/pivotal/track/TrackingStatusRepository.java
@@ -2,10 +2,10 @@ package io.pivotal.track;
 
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel="tracking-statii", path="tracking-statii")
-public interface TrackingStatusRepository extends JpaRepository<TrackingStatus, UUID> {
+public interface TrackingStatusRepository extends CrudRepository<TrackingStatus, UUID> {
 
 }
